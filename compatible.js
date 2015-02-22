4e79f33bebc44bd79149356f882f2a175133bf4e
nelsonic <contact.nelsonic@gmail.com>
2015-02-22 23:40:35 +0000

    cannot get twin-bcrypt to work! https://github.com/fpirsch/twin-bcrypt/issues/3

---
+++ compatible.js
@@ -0,0 +1,8 @@
+var twinbcrypt = require('twin-bcrypt');
+var bcrypt       = require('bcrypt');
+var assert       = require('assert');
+
+var salt  = twinbcrypt.genSalt(12);
+var hash = twinbcrypt.hashSync('hello', salt);
+
+assert.equal(bcrypt.compareSync("hello", hash) === true, "twin-bcrypt is compatible with bcrypt");
